b988d5f7521984ef0d9c139313514caa7c6b4c91
TonySD <ahtoxa.b@gmail.com>
2025-05-18 12:30:37 +0300

    Impl: implemented configuration parsing

---
+++ src/configuration.rs
@@ -0,0 +1,67 @@
+use clap::{Parser, Subcommand};
+
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None, subcommand_help_heading = "Available Subcommands", disable_help_subcommand = true)]
+pub struct Configuration {
+    #[arg(short, long)]
+    pub verbose: bool,
+
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Commands {
+    Inject(InjectArgs),
+    Macro(MacroArgs),
+    Create(CreateArgs),
+}
+
+#[derive(Parser, Debug)]
+pub struct InjectArgs {
+    #[arg(value_parser)]
+    pub input_docx_file: String,
+
+    // TODO: inject macro to document, implement not remote injection technique
+    #[arg(value_parser)]
+    pub macro_path: String,
+
+    #[arg(short, long, default_value = "patched_document.docx")]
+    pub output_docx_file: String,
+}
+
+#[derive(Parser, Debug)]
+pub struct MacroArgs {
+    #[arg(short='f', long="file", required_unless_present="source_code")]
+    pub input_file: Option<String>,
+
+    #[arg(short='s', long, required_unless_present="input_file")]
+    pub source_code: Option<String>,
+}
+
+#[derive(Parser, Debug)]
+pub struct CreateArgs {
+    #[command(subcommand)]
+    pub command: CreateCommands,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum CreateCommands {
+    Empty {
+        #[arg(short='d', long)]
+        output_directory: String,
+    },
+
+    Full {
+        #[arg(short='d', long)]
+        output_directory: String
+    },
+
+    #[command(name="fulldoc")]
+    FullDoc {
+        #[arg(short='d', long)]
+        output_directory: String
+    },
+}
+
