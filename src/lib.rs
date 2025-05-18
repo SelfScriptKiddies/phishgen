4312e320c84eb200f7754f18b4aeb428cf350b99
TonySD <ahtoxa.b@gmail.com>
2025-05-19 00:06:49 +0300

    Impl: create command

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use configuration::{Configuration, Commands, InjectArgs, MacroArgs, CreateArgs};
+use configuration::*;
 use log::{debug, error, info};
 pub mod configuration;
 
@@ -24,7 +24,32 @@ fn generate_macro(args: MacroArgs) -> Result<(), std::io::Error> {
     Ok(())
 }
 
-fn create(args: CreateArgs) -> Result<(), std::io::Error> {
-    info!("Creating document");
+struct Document {
+    path: String,
+    content: Vec<u8>,
+}
+
+fn create(args: CreateArgs) -> Result<(), std::io::Error> {    
+    let document: Document = match args.command {
+        CreateCommands::Empty { output_directory } => Document {
+            path: format!("{}/harmless_pattern.dotx", output_directory),
+            content: include_bytes!("../resources/examples/harmless_pattern.dotx").to_vec(),
+        },
+        CreateCommands::Full { output_directory } => Document {
+            path: format!("{}/full_resume_pattern.dotx", output_directory),
+            content: include_bytes!("../resources/examples/full_resume_pattern.dotx").to_vec(),
+        },
+        CreateCommands::FullDoc { output_directory } => Document {
+            path: format!("{}/resume_document.docx", output_directory),
+            content: include_bytes!("../resources/examples/resume_document.docx").to_vec(),
+        },
+    };
+
+    info!("Creating document {}", document.path);
+
+    std::fs::write(document.path, document.content)?;
+
+    info!("Document created successfully");
+    
     Ok(())
 }
